264d9036bacea45f06ff5a3a50918fdfd716b3aa
filippoaleotti <filippoaleotti@yahoo.it>
2020-11-20 18:58:34 +0100

    change html theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,7 @@ import os
 import sys
 
 sys.path.insert(0, os.path.abspath("../.."))
-import sphinx_theme
+import sphinx_rtd_theme
 
 # -- Project information -----------------------------------------------------
 
@@ -36,6 +36,7 @@ extensions = [
     "sphinx.ext.napoleon",
     "nbsphinx",
     "sphinxcontrib.bibtex",
+    "sphinx_rtd_theme",
 ]
 napoleon_google_docstring = True
 
@@ -53,15 +54,9 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "stanford_theme"
-html_theme_path = [sphinx_theme.get_html_theme_path("stanford-theme")]
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = []
-
-
-def setup(app):
-    app.add_css_file("css/theme.css")
-    app.add_js_file("js/theme.js")
