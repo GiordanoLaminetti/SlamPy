c574672e8591e6672fe97319051e56472944bc19
Giordano Laminetti <giordano.laminetti.gl@gmail.com>
2020-12-02 00:20:56 +0100

    add __init__ docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -43,6 +43,9 @@ napoleon_google_docstring = True
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
 
+# Add docstring of __init__ methods and class method
+autoclass_content = "both"
+
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
